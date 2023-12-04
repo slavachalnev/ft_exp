a12200003364f9831452ba33700f4ef5859ab6f4
Slava Chalnev <slava.chalnev@gmail.com>
2023-12-04 12:53:56 +1100

    Compare to autoencoder

--- make_json.py
+++ make_json.py
@@ -3,6 +3,8 @@ import json
 import pandas as pd
 import torch
 from tqdm import tqdm
+import matplotlib
+import matplotlib.pyplot as plt
 
 
 SPACE = "·"
@@ -79,4 +81,60 @@ def single_neuron(tokens: torch.Tensor, acts: torch.Tensor, ft_id, original_mode
     }
     return res
 
+
+def style_snippet(snippet_idx, act_data):
+    tokens_with_activations = act_data['snippets'][snippet_idx]["token_activation_pairs"]
+    max_act = act_data['snippets'][snippet_idx]["max_activation"]
+    if max_act == 0:
+        return ""
+    
+    # Function to map activation to color
+    def activation_to_color(activation):
+        if activation < 0:
+            return '#FFFFFF'
+        normalized_activation = activation / max_act*0.6
+        return plt.cm.Reds(normalized_activation)
+    
+    styled_text = ''.join(f'<span style="background-color: {matplotlib.colors.rgb2hex(activation_to_color(activation))}; margin-right: 0px;">{token}</span>'
+                          for token, activation in tokens_with_activations)
+    return styled_text
+
+
+def save_snippets_to_html(feature_id, snippets_data, first_batch, feature_freq, batch_size, n_batches, results_dir):
+    filename = f"{results_dir}/neuron_{feature_id}_highlights.html"
+    with open(filename, "w") as file:
+        file.write(f"<html><head><title>Neuron {feature_id} Highlights</title></head><body>")
+        file.write(f"<h1>Neuron {feature_id} Highlights</h1>")
+        file.write(f"<div><strong>Number of batches processed:</strong> {n_batches}</div>")
+        file.write(f"<div><strong>Batch size:</strong> {batch_size}</div>")
+        file.write(f"<div><strong>Feature frequency:</strong> {feature_freq:.6f}</div>")
+        file.write("<hr>")
+        if feature_freq > 0:
+
+            for snippet_idx, snippet in enumerate(snippets_data['snippets']):
+                styled_text = style_snippet(snippet_idx, snippets_data)
+                if not styled_text:
+                    continue
+                snippet_info = (f'<div style="word-wrap: break-word; margin-bottom: 10px;">'
+                                f'<strong>Snippet number:</strong> {snippet_idx}<br>'
+                                f'<strong>Max activation:</strong> {snippet["max_activation"]:.6f}<br>'
+                                f'{styled_text}</div>')
+                file.write(snippet_info)
+
+        file.write("<hr>")
+            
+        if first_batch and feature_freq > 0:
+            # Append first batch results
+            file.write("<h2>First Batch Results</h2>")
+            for snippet_idx, snippet in enumerate(first_batch['snippets']):
+                styled_text = style_snippet(snippet_idx, first_batch)
+                if not styled_text:
+                    continue
+                snippet_info = (f'<div style="word-wrap: break-word; margin-bottom: 10px;">'
+                                f'<strong>Snippet number (First Batch):</strong> {snippet_idx}<br>'
+                                f'<strong>Max activation (First Batch):</strong> {snippet["max_activation"]:.6f}<br>'
+                                f'{styled_text}</div>')
+                file.write(snippet_info)
+
+        file.write("</body></html>")
 
